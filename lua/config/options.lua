811eaed7d7a6725f1acec9808b8fe6d6ffa1be45
An Cao <ancao@remitano.com>
2023-11-26 14:33:57 +0700

    Config splitright and splitbelow

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,6 @@ vim.opt.relativenumber = true -- Show the line number relative to the line with 
 vim.opt.shiftwidth = 0 -- Number of spaces to use for each step of (auto)indent. When zero the 'tabstop' value will be used.
 vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for.
 vim.opt.expandtab = true -- In Insert mode: Use the appropriate number of space to insert a <Tab>.
+
+vim.opt.splitright = true -- Splitting a window will put the new window right of the current one.
+vim.opt.splitbelow = true -- Splitting a window will put the new window below the current one.
