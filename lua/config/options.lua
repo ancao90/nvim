0ef8ab15d9253fd8a78e45e8137493db6d22c0d8
An Cao <ancao@remitano.com>
2023-11-26 10:31:50 +0700

    Add options

---
+++ lua/config/options.lua
@@ -0,0 +1,6 @@
+vim.opt.number = true -- Precede each line with its line number
+vim.opt.relativenumber = true -- Show the line number relative to the line with the cursor in front of each line.
+
+vim.opt.shiftwidth = 0 -- Number of spaces to use for each step of (auto)indent. When zero the 'tabstop' value will be used.
+vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for.
+vim.opt.expandtab = true -- In Insert mode: Use the appropriate number of space to insert a <Tab>.
