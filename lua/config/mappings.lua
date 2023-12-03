4c348dc64be0a0b670f3c52fbbcc465822f9efde
An Cao <an.cao@andpad.co.jp>
2023-12-03 09:36:36 +0700

    Improve mapping leader+tr

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -45,7 +45,7 @@ vim.keymap.set("n", "<Leader>h", "<cmd>noh<CR>", { noremap = true, silent = true
 vim.keymap.set("n", "<leader>te", "0wdf:wxa= <esc>wdwf}x")
 
 -- map leader+tr to change one-liner let rspec to removing let
-vim.keymap.set("n", "<leader>tr", "0df{<esc>f}x==")
+vim.keymap.set("n", "<leader>tr", "0df{<esc>f}x^==")
 
 -- map leader+f to format file
 vim.keymap.set("n", "<Leader>f", "<cmd>lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
