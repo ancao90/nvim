1c941906a742d4c8028249a1ae6ce0c1bab4fd7f
An Cao <an.cao@andpad.co.jp>
2023-12-03 14:03:06 +0700

    Auto restart Lsp after changing rubocop config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
       vim.highlight.on_yank({ timeout = 200 })
    end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { ".rubocop_todo.yml", ".rubocop.yml" },
+  callback = function()
+    vim.cmd([[LspRestart]])
+    vim.cmd([[echo 'Lsp Restarted.']])
+  end
+})
