ac98dbbe2606de1f2e5cf342ed7d7829ece4ce76
An Cao <an.cao@andpad.co.jp>
2023-12-05 21:24:39 +0700

    Add snippet

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -12,6 +12,9 @@ return {
     local cmp = require("cmp")
 
     local luasnip = require("luasnip")
+    luasnip.add_snippets("gitcommit", {
+      luasnip.snippet("fixRSpecLetSetup", { luasnip.text_node("Fix rubocop offenses RSpec/LetSetup in ") })
+    })
 
     require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -42,54 +45,3 @@ return {
     })
   end
 }
--- return {
---   {
---     'dcampos/cmp-snippy',
---     config = function()
---       require 'cmp'.setup {
---         snippet = {
---           expand = function(args)
---             require 'snippy'.expand_snippet(args.body)
---           end
---         },
-
---         sources = {
---           { name = 'snippy' }
---         }
---       }
---     end
---   },
---   {
---     'dcampos/nvim-snippy',
---     config = function()
---       require('snippy').setup({
---         snippet_dirs = '~/.config/nvim/snippets',
---         local_snippet_dir = '.snippets',
---         hl_group = 'Search',
---         enable_auto = false,
---         scopes = {
---           _ = {},
-
---           lua = function(scopes)
---             if vim.api.nvim_buf_get_name(0):find('_spec.lua$') then
---               table.insert(scopes, 'busted')
---             end
---             return scopes
---           end,
-
---           lua = { '_', 'lua', 'html', 'javascript' },
---           gitcommit = { 'gitcommit' },
---         },
---         mappings = {
---           is = {
---             ["<Tab>"] = "expand_or_advance",
---             ["<S-Tab>"] = "previous",
---           },
---           nx = {
---             ["<leader>x"] = "cut_text",
---           },
---         },
---       })
---     end
---   }
--- }
