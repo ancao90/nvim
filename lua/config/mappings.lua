3ec24317ebae0a92078b42844820430795d44c93
An Cao <an.cao@andpad.co.jp>
2023-12-15 15:51:29 +0700

    Format code

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,45 +1,45 @@
 vim.g.mapleader = " "
 
 -- map leader+w to save current file in normal mode
-vim.keymap.set("n", "<Leader>w", ":HopWord<CR>", { noremap = true, silent = true}) 
+vim.keymap.set("n", "<Leader>w", ":HopWord<CR>", { noremap = true, silent = true })
 
 -- map Ctrl+S to save current file in normal mode and visual and select modes
-vim.keymap.set({"n", "v"}, "<C-S>", ":write<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-S>", ":write<CR>", { noremap = true, silent = true })
 
 -- map Ctrl+S to save current file in insert mode
-vim.keymap.set("i", "<C-S>", "<esc>:write<CR>", { noremap = true, silent = true})
+vim.keymap.set("i", "<C-S>", "<esc>:write<CR>", { noremap = true, silent = true })
 
 --map leader+y to copy to system clipboard in normal, visual and select modes
-vim.keymap.set({"n", "v"}, "<Leader>y", '"+y', { noremap = true, silent = false })
+vim.keymap.set({ "n", "v" }, "<Leader>y", '"+y', { noremap = true, silent = false })
 
 
 -- map Ctrl+L to move the cursor to the right split
-vim.keymap.set("n", "<C-L>", "<C-W>l", { noremap = true, silent = true})
+vim.keymap.set("n", "<C-L>", "<C-W>l", { noremap = true, silent = true })
 
 -- map Ctrl+H to move the cursor to the left split
-vim.keymap.set("n", "<C-H>", "<C-W>h", { noremap = true, silent = true})
+vim.keymap.set("n", "<C-H>", "<C-W>h", { noremap = true, silent = true })
 
 
 -- map Ctrl+K to move the cursor to the above split
-vim.keymap.set("n", "<C-K>", "<C-W>k", { noremap = true, silent = true})
+vim.keymap.set("n", "<C-K>", "<C-W>k", { noremap = true, silent = true })
 
 -- map Ctrl+J to move the cursor to the below split
-vim.keymap.set("n", "<C-J>", "<C-W>j", { noremap = true, silent = true})
+vim.keymap.set("n", "<C-J>", "<C-W>j", { noremap = true, silent = true })
 
 -- map Shift+L to move the cursor to the right tab
-vim.keymap.set("n", "<S-L>", "<cmd>tabn<CR>", { noremap = true, silent = true})
+vim.keymap.set("n", "<S-L>", "<cmd>tabn<CR>", { noremap = true, silent = true })
 
 -- map Shift+H to move the cursor to the left tab
-vim.keymap.set("n", "<S-H>", "<cmd>tabp<CR>", { noremap = true, silent = true})
+vim.keymap.set("n", "<S-H>", "<cmd>tabp<CR>", { noremap = true, silent = true })
 
 -- map Ctrl+C to close the current window
-vim.keymap.set("n", "<C-C>", "<cmd>close<CR>", { noremap = true, silent = true})
+vim.keymap.set("n", "<C-C>", "<cmd>close<CR>", { noremap = true, silent = true })
 
 -- map Ctrl+Q to quit the current window
-vim.keymap.set("n", "<C-Q>", "<cmd>quit<CR>", { noremap = true, silent = true})
+vim.keymap.set("n", "<C-Q>", "<cmd>quit<CR>", { noremap = true, silent = true })
 
 -- map leader+h to hide search highlight
-vim.keymap.set("n", "<Leader>h", "<cmd>noh<CR>", { noremap = true, silent = true})
+vim.keymap.set("n", "<Leader>h", "<cmd>noh<CR>", { noremap = true, silent = true })
 
 -- map leader+te to change one-liner let rspec to variable
 vim.keymap.set("n", "<leader>te", "0wdf:wxa= <esc>wdwf}x")
@@ -62,20 +62,23 @@ vim.keymap.set("n", "<C-S-j>", "<cmd>resize -2<CR>", { noremap = true, silent = 
 --
 
 -- map Ctrl+P to find files
-vim.keymap.set("n", "<C-P>", "<cmd>lua require('fzf-lua').files({ fzf_opts = {['--layout'] = 'default'} })<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-P>", "<cmd>lua require('fzf-lua').files({ fzf_opts = {['--layout'] = 'default'} })<CR>",
+  { noremap = true, silent = true })
 
 -- Find git branches
-vim.keymap.set("n", "<C-G>b", "<cmd>lua require('fzf-lua').git_branches()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>gb", "<cmd>lua require('fzf-lua').git_branches()<CR>", { noremap = true, silent = true })
 -- Find git stashes
-vim.keymap.set("n", "<C-G>s", "<cmd>lua require('fzf-lua').git_stash()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>gs", "<cmd>lua require('fzf-lua').git_stash()<CR>", { noremap = true, silent = true })
 -- View git status
-vim.keymap.set("n", "<C-G>t", "<cmd>lua require('fzf-lua').git_status()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>gt", "<cmd>lua require('fzf-lua').git_status()<CR>", { noremap = true, silent = true })
 -- View git commits
 vim.keymap.set("n", "<Leader>gc", "<cmd>lua require('fzf-lua').git_commits()<CR>", { noremap = true, silent = true })
 
 -- map leader+s to search files
-vim.keymap.set("n", "<Leader>s", "<cmd>lua require('fzf-lua').live_grep({ fzf_opts = {['--layout'] = 'default'} })<CR>", { noremap = true, silent = true })
-vim.keymap.set("v", "<Leader>s", "<cmd>lua require('fzf-lua').grep_visual({ fzf_opts = {['--layout'] = 'default'} })<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>s", "<cmd>lua require('fzf-lua').live_grep({ fzf_opts = {['--layout'] = 'default'} })<CR>",
+  { noremap = true, silent = true })
+vim.keymap.set("v", "<Leader>s", "<cmd>lua require('fzf-lua').grep_visual({ fzf_opts = {['--layout'] = 'default'} })<CR>",
+  { noremap = true, silent = true })
 
 
 --
