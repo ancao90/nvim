bbd3b781c99ceb884e248a569027e76c69463cf2
An Cao <ancao@remitano.com>
2024-01-03 15:22:58 +0700

    Support more patterns for go to Rails files

--- lua/mm.lua
+++ lua/mm.lua
@@ -1,14 +1,33 @@
 local M = {}
 
 M.DeleteGitBranch = function(selected)
-  vim.cmd(string.format("G branch -d %s | echo", selected[1]))
+  vim.cmd(string.format('G branch -d %s | echo', selected[1]))
   -- vim.cmd("echo 'hello'")
 end
 
-M.alternate = function(path)
-  local _, _, model = string.find(path, "app/models/([%a_]+).rb")
+M.alternate = function(path, cword)
+  local _, _, model = string.find(path, 'app/models/([%a_]+).rb')
 
-  return "spec/models/"..model.."_spec.rb"
+  if model == nil then
+    local _, _, controller = string.find(path, 'app/controllers/([%a_]+)_controller.rb')
+    if controller == nil then
+      local _, _, controller = string.find(path, 'spec/requests/([%a_]+)/%a+_spec.rb')
+      if controller == nil then
+        local _, _, model = string.find(path, 'spec/models/([%a_]+)_spec.rb')
+        if model == nil then
+          return nil
+        else
+          return 'app/models/' .. model .. '.rb'
+        end
+      else
+        return 'app/controllers/' .. controller .. '_controller.rb'
+      end
+    else
+      return 'spec/requests/' .. controller .. '/' .. cword .. '_spec.rb'
+    end
+  else
+    return 'spec/models/' .. model .. '_spec.rb'
+  end
 end
 
 return M
--- lua/mm_spec.lua
+++ lua/mm_spec.lua
@@ -19,5 +19,41 @@ describe('mm', function()
         )
       end)
     end)
+
+    context('when the given path is not supported', function()
+      it('returns nil', function()
+        assert.are.same(
+          mm.alternate('app/services/user.rb'),
+          nil
+        )
+      end)
+    end)
+
+    context('when the given path is a controller path', function()
+      it('returns alternative path', function()
+        assert.are.same(
+          mm.alternate('app/controllers/users_controller.rb', 'show'),
+          'spec/requests/users/show_spec.rb'
+        )
+      end)
+    end)
+
+    context('with spec request path', function()
+      it('returns controller path', function()
+        assert.are.same(
+          mm.alternate('spec/requests/users/show_spec.rb'),
+          'app/controllers/users_controller.rb'
+        )
+      end)
+    end)
+
+    context('with spec model path', function()
+      it('returns model path', function()
+        assert.are.same(
+          mm.alternate('spec/models/user_spec.rb'),
+          'app/models/user.rb'
+        )
+      end)
+    end)
   end)
 end)
